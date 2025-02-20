81e290cad8df4546199b0c31040be1003bc11be7
Sam <samuelasheppard@gmail.com>
2025-02-20 20:32:02 +0000

    fixed issues with ts and jest imports

--- index.ts
+++ index.ts
@@ -7,7 +7,7 @@ import errorLogger from "./src/middleware/errorHandler";
 const cors = require("cors");
 
 const app: Express = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(bodyParser.json());
