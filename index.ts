851f05de54655bfdc6542b0a9f3827d3776ee454
Sam <samuelasheppard@gmail.com>
2025-02-24 17:02:55 +0000

    added build command for deployment

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import express, { Express } from "express";
+import express, { Express, Response, Request, NextFunction } from "express";
 import * as bodyParser from "body-parser";
 import router from "./src/routers/router";
 import logger from "./src/middleware/logRequest";
@@ -11,7 +11,10 @@ const port = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(bodyParser.json());
-
+//health check
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).send({ status: "ok" });
+});
 app.use(logger);
 
 app.use(router);
